01d00f5027dd6c3ed5f2515b7d2d54a049b17047
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-11-03 20:05:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const getData = (day) => {
+    const headers = {
+      "x-rapidapi-host": "rarible-data-scraper.p.rapidapi.com",
+      "x-rapidapi-key": "62f1e07a06mshb58178b98356480p135ff4jsnbcfab5493d0e",
+    };
+
+    axios
+      .get(
+        `https://rarible-data-scraper.p.rapidapi.com/top_collection/${day}/100`,
+        {
+          headers: headers,
+        }
+      )
+      .then((res) => {
+        const filtredData = [];
+
+        res.data.list.forEach((item) => {
+          if (item.pic) {
+            const Newitem = {
+              id: item.id,
+              name: item.name,
+              link: `https://rarible.com/collection/${item.id}`,
+              pic: item.pic.replace("ipfs://", ""),
+            };
+
+            filtredData.push(Newitem);
+          }
+        });
+
+        setData(filtredData);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => getData(1), []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Top collections</h1>
+      <div className="filter">
+        <span>
+          Day <output id="output">0</output>
+        </span>
+        <input
+          type="range"
+          onInput={(e) => {
+            var number = e.target.value;
+
+            document.getElementById("output").value = number;
+            getData(number);
+          }}
+          name="vol"
+          default="1"
+          min="1"
+          max="30"
+        />
+      </div>
+      <div className="list">
+        {data.map((nft, key) => (
+          <div className="nft" key={key}>
+            <a href={nft.link} target="_blank" rel="noreferrer">
+              <img
+                src={`https://ipfs.io/${nft.pic.replace("ipfs://", "")}`}
+                alt={nft.symbol}
+                title={nft.symbol}
+              />
+            </a>
+            <h2>{nft.name}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
